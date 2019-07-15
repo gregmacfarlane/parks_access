e3ce06b51e910edb0e976c03faad856e269eade2
Greg Macfarlane <gregmacfarlane@gmail.com>
2019-07-15 14:53:02 -0600

    Revise open spaces builder
    Calculate trails, playgrounds, courts, etc.

---
+++ parks_access/ui.R
@@ -0,0 +1,68 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(leaflet)
+
+vars <- c(
+    "Is SuperZIP?" = "superzip",
+    "Centile score" = "centile",
+    "College education" = "college",
+    "Median income" = "income",
+    "Population" = "adultpop"
+)
+
+
+navbarPage(
+    "Park Accessibility", id="nav",
+    
+    tabPanel(
+        "Interactive map",
+        div(class="outer",
+            
+            tags$head(
+                # Include our custom CSS
+                includeCSS("styles.css"),
+                includeScript("gomap.js")
+            ),
+            
+            # If not using custom CSS, set height of leafletOutput to a number instead of percent
+            leafletOutput("map", width="100%", height="100%"),
+            
+            # Shiny versions prior to 0.11 should use class = "modal" instead.
+            absolutePanel(
+                id = "controls", class = "panel panel-default", fixed = TRUE,
+                top = 60, left = 30, right = "auto", bottom = "auto",
+                width = 330, height = "auto",
+                
+                h2("Tract Accessibility"),
+                
+                sliderInput("distance_coef", "Distance Effect", min = -5, max = -0.01,
+                            step = 0.1, value = -1.9),
+                sliderInput("size_coef", "Size Effect", min = 0.01, max = 5,
+                            step = 0.1, value = .3),
+                conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                                 # Only prompt for threshold when coloring or sizing by superzip
+                                 numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+                ),
+                
+                plotOutput("histCentile", height = 200),
+                plotOutput("scatterObesity", height = 250)
+            ),
+            
+            tags$div(
+                id="cite",
+                'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
+            )
+        )
+    ),
+    
+    conditionalPanel("false", icon("crosshair"))
+    
+)
